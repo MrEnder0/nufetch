68795483f6b520157c43c301c218c5a149de033a
Mr.Ender <endermobtrey@gmail.com>
2023-04-05 18:52:29 +0000

    fix again :/ me dumb

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -1,10 +1,8 @@
 #Test for existing install
 NUFETCH=~/.nufetch/nufetch.nu
 if [ -f "$NUFETCH" ]; then
-    echo "$NUFETCH already exists would you like to re-install (1) or un-install (2): default 1"
-
-    read option
-    if $option == "1" || $option == ""; then
+    read -p "$NUFETCH already exists would you like to re-install (1) or un-install (2): default 1" some_input
+    if [ $option = "1" || $option = "" ]; then
         rm -r ~/.nufetch/*
         curl -o ~/.nufetch/nufetch.nu https://raw.githubusercontent.com/MrEnder0/nufetch/master/src/nufetch.nu
         curl -o ~/.nufetch/nufetch_config.nu https://raw.githubusercontent.com/MrEnder0/nufetch/master/src/nufetch_config.nu
